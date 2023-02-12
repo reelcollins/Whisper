f5692b254b423d6d5b3af4ab9d9319cecf3a0765
reelcollins <elijahcollins254@gmail.com>
2023-02-12 13:40:16 +0300

    preprocessing audio

--- Whisper/audio.py
+++ Whisper/audio.py
@@ -5,7 +5,7 @@ from typing import Union
 import ffmpeg
 import numpy as np
 import torch
-import tor ch.nn.functional as F
+import torch.nn.functional as F
 
 from utils import exact_div
 
@@ -22,5 +22,46 @@ N_FRAMES =  exact_div(N_SAMPLES, HOP_LENGTH)  # 3000: number of frames in a mel 
 
 def load_audio(file: str, sr: int = SAMPLE_RATE):
   """
+  open an audio file and read as mono waveform, resampling as necessary
+
+  Parameters
+  ----------
+  file: str
+    The audio file to open
+  
+  str: int
+    The sample rate to resample the audion if necessary
+
+
+  Returns
+  -------
+  A NumPy array containing the audio waveform, in float32 dtype.
   
   """
+  try:
+    # This launches a subprocess to decide audio while down-mixing and resampling as necessary.
+    # Requires the ffmpeg CLI and `ffmpeg-python` package to be installed.
+    out, _ = (
+      ffmpeg.input(file, threads=0)
+      .output("-", format="s16le", acodec="pcm_s16le", ac=1, ar=sr)
+      .run(cmd=["ffmpeg", "-nostdin"], capture_stdout=True, capture_stderr=True)
+    )
+  except ffmpeg.Error as e:
+    raise RuntimeError(f"Failed to load audio: {e.stderr.decode()}") from e
+  
+  return np.frombuffer(out, np.int16).flatten().astype(np.float32) / 32768.0
+
+
+
+def pad_or_trim(array, length: int = N_SAMPLES, *, axis: int = -1):
+  """
+  Pad or trim the audio array to N_SAMPLES, as expected by the encoder.
+  """
+  if torch.is_tensor(array):
+    if array.shape[axis] > length:
+      array = array.index_select(dim=axis, index=torch.arange(length, device=array.device))
+
+    if array.shape[axis] < length:
+      pad_widths = [(0, 0)] * array.ndim
+      pad_widths[axis] = (0, length - array.shape[axis])
+      array = F.pad(array, [pad for sizes in pad_widths[::-1] for pad in sizes])
