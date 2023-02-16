afea7e5d7f8d69f62090bc4e4ca8f7c9fe884cd2
reelcollins <elijahcollins254@gmail.com>
2023-02-16 18:16:20 +0300

    decoding

--- Whisper/decoding.py
+++ Whisper/decoding.py
@@ -650,3 +650,73 @@ def run(self, mel: Tensor) -> List[DecodingResult]:
 
     # call the main sampling loop
     tokens, sum_logprobs, no_speech_probs = self._main_loop(audio_features, tokens)
+    
+    # reshape the tensors to have (n_audio, n_group) as the first two dimensions
+    audio_feautures = audio_features[:: self.n_group]
+    no_speech_probs = no_speech_probs[:: self.n_group]
+    assert audio_feautures.shape[0] == len(no_speech_probs) == n_audio
+
+    tokens = tokens.reshape(n_audio, self.n_group, -1)
+    sum_logprobs = sum_logprobs.reshape(n_audio, self.n_group)
+
+    # get the final candidates for each group, and slice between the first sampled token and EOT
+    tokens, sum_logprobs = self.decoder.finalize(tokens, sum_logprobs)
+    tokens: List[List[Tensor]] = [
+        [t[self.sample_begin : (t == tokenizer.eot).nonzero()[0, 0]] for t in s] for s in tokens
+    ]
+
+    # select the top-ranked sample in each group
+    selected = self.sequence_ranker.rank(tokens, sum_logprobs)
+    tokens: List[List[int]] = [t[i].tolist() for i, t in zip(selected, tokens)]
+    texts: List[str] = [tokenizer.decode(t).strip() for t in tokens]
+
+    sum_logprobs: List[float] = [lp[i] for i, lp in zip(selected, sum_logprobs)]
+    avg_logprobs: List[float] = [lp / (len(t) + 1) for t, lp in zip(tokens, sum_logprobs)]
+
+    fields = (texts, languages, tokens, audio_features, avg_logprobs, no_speech_probs)
+    if len(set(map(len, fields))) != 1:
+        raise RuntimeError(f"inconsistent result lengths: {list(map(len, fields))}")
+    
+    return [
+        DecodingResult(
+        audio_features=features,
+        language=language,
+        tokens=tokens,
+        text=text,
+        avg_logprob=avg_logprob,
+        no_speech_prob=no_speech_prob,
+        temperature=self.options.temperature,
+        compression_ratio=compression_ratio(text)
+
+        )
+        for text, language, tokens, features, avg_logprob, no_speech_prob in zip(*fields)
+    ]
+
+
+@torch.no_grad()
+def decode(model: "Whisper", mel: Tensor, options: DecodingOptions = DecodingOptions()) -> Union[DecodingResult, List[DecodingResult]]:
+    """
+    Performs decoding of 30-second audio segment(s), provided as Mel spectogram(s).
+    
+    Parameters
+    ----------
+    model: Whisper
+        the whisper model instance
+        
+    mel: torch.Tensor, shape = (80, 3000) or (*, 80, 3000)
+        A tensor containing the Mel spectrogram(s)
+        
+    options: DecodingOptions
+        A dataclass that contains all necessary options for decoding 30-second segments
+        
+    Returns
+    -------
+    result: Union[DecodingResult, List[DecodingResult]]
+        The result(s) of decoding contained in `DecodingResult` dataclass instance(s)
+    """
+    if single := mel.ndim == 2:
+        mel = mel.unsqueeze(0)
+    
+    result = DecodingTask(model, options).run(mel)
+
+    return result[0] if single else result
